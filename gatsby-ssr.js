5c9740a0bf9da728341127fe570ade0c126d4d9a
ksh <iltk2000@naver.com>
2021-12-29 21:55:43 +0900

    [feat] devtools console창 넣어보기

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,3 +5,17 @@
  */
 
 // You can delete this file if you're not using it
+const React = require("react")
+
+exports.onRenderBody = () => {
+  if (window.console !== undefined) {
+    const cssRule = "font-size:40px;"
+    const githubCss = "color:skyblue"
+    setTimeout(() => {
+      console.log.bind(console, "Hello World", cssRule)
+    }, 0)
+    setTimeout(() => {
+      console.log.bind(console, "https://github.com/bluelion2", githubCss)
+    }, 0)
+  }
+}
