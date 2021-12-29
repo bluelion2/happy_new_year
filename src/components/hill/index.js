c3930a9103cd6a10b2abb4edc1552d95aa05c12c
ksh <iltk2000@naver.com>
2021-12-29 21:40:26 +0900

    [fix] bitly에 쿼리파람이 안되서 롤백

--- src/components/hill/index.js
+++ src/components/hill/index.js
@@ -4,32 +4,13 @@ import "./index.css"
 
 const Hill = () => {
   const onClick = () => {
-    const name =
-      window.prompt(
-        "받으실 분 성함을 입력해주세요. \n입력하지 않으면 이름없이 생성됩니다."
-      ) || ""
-
     const url = isMobile
-      ? `https://bit.ly/3EzJUo3?name=${name}`
-      : `https://every-happy-new-year.vercel.app?name=${name}`
-
-    console.log("name", name)
+      ? "https://bit.ly/3EzJUo3"
+      : "https://every-happy-new-year.vercel.app"
     navigator.clipboard.writeText(url).then(() => {
-      const text = name
-        ? `링크가 만들어 졌습니다. \n${name}님에게 전달해주세요.`
-        : "링크가 복사되었습니다. \n다른 분들에게도 인사해요."
-      alert(text)
+      alert("링크가 복사되었습니다. \n다른 분들에게도 인사해요.")
     })
   }
-
-  // const onClick = () => {
-  //   const url = isMobile
-  //     ? "https://bit.ly/3EzJUo3"
-  //     : "https://every-happy-new-year.vercel.app"
-  //   navigator.clipboard.writeText(url).then(() => {
-  //     alert("링크가 복사되었습니다. \n다른 분들에게도 인사해요.")
-  //   })
-  // }
 
   return (
     <footer className="hill">
