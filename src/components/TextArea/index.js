b2f1b16f737c2f379af3566e90b4dfaf7292c299
ksh <iltk2000@naver.com>
2021-12-29 17:25:45 +0900

    [fix] 처음에 텍스트만 보이는 이슈 해결

--- src/components/TextArea/index.js
+++ src/components/TextArea/index.js
@@ -3,16 +3,23 @@ import styled from "styled-components"
 
 const TextArea = () => {
   const [text, setText] = React.useState("")
+  const [visible, setVisible] = React.useState(false)
+
   React.useEffect(() => {
     const i = Math.floor(Math.random() * 10)
     setText(guide[i])
+    setVisible(true)
   }, [])
 
   return (
     <StyledTextArea>
-      <h2>{new Date().getFullYear()}</h2>
-      <h1>근하신년</h1>
-      <p>{text}</p>
+      {visible && (
+        <>
+          <h2>{new Date().getFullYear()}</h2>
+          <h1>근하신년</h1>
+          <p>{text}</p>
+        </>
+      )}
     </StyledTextArea>
   )
 }
@@ -23,11 +30,12 @@ const guide = [
   "새해 복 많이 받으세요.",
   "좋은일만 가득하길 바랍니다.",
   "항상 건강하시고 행복한 일만 가득하길..",
+  "복권당첨.청약당첨.집값상승",
+  "무병장수, 운수대통 하시길 바랍니다.",
   "새해 복 많이 받으세요.",
   "좋은일만 가득하길 바랍니다.",
   "항상 건강하시고 행복한 일만 가득하길..",
   "새해 복 많이 받으세요.",
-  "복권당첨.청약당첨.집값상승",
   "항상 건강하시고 행복한 일만 가득하길..",
   "새해 복 많이 받으세요.",
 ]
