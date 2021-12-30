a86fbc0a6732fbe98efba7bd5ccc7fb4fe8f928b
ksh <iltk2000@naver.com>
2021-12-30 14:35:20 +0900

    [fix] cloud animation fix

--- src/components/sun/index.js
+++ src/components/sun/index.js
@@ -8,7 +8,17 @@ const Sun = () => {
       <div className="sun" />
       <StaticImage className="cloud" src="../../images/cloud.png" alt="구름" />
       <StaticImage
+        className="cloud-1"
+        src="../../images/cloud.png"
+        alt="구름"
+      />
+      <StaticImage
         className="white-cloud"
+        src="../../images/white-cloud.png"
+        alt="구름"
+      />
+      <StaticImage
+        className="white-cloud-1"
         src="../../images/white-cloud.png"
         alt="구름"
       />
