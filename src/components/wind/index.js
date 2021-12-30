04812cd133d4ce4688f963211125cf5d05871774
ksh <iltk2000@naver.com>
2021-12-30 12:59:43 +0900

    [fix] png 교체

--- src/components/wind/index.js
+++ src/components/wind/index.js
@@ -5,41 +5,36 @@ import "./index.css"
 const Wind = () => {
   return (
     <div className="wind">
-      {/* <StaticImage
-        className="wind-3"
-        src="../../images/wind.svg"
-        alt="wind-svg"
-      />
-      <StaticImage
-        className="wind-4"
-        src="../../images/wind.svg"
-        alt="wind-svg"
-      />
+      <StaticImage className="wind-1" src="../../images/wind.png" alt="바람" />
+      <StaticImage className="wind-2" src="../../images/wind.png" alt="바람" />
+      <StaticImage className="wind-3" src="../../images/wind.png" alt="바람" />
+      <StaticImage className="wind-4" src="../../images/wind.png" alt="바람" />
+
       <StaticImage
         className="cloud-1"
-        src="../../images/cloud.svg"
-        alt="cloud-svg"
+        src="../../images/cloud.png"
+        alt="구름"
       />
       <StaticImage
         className="cloud-2"
-        src="../../images/cloud.svg"
-        alt="cloud-svg"
+        src="../../images/cloud.png"
+        alt="구름"
       />
       <StaticImage
         className="cloud-3"
-        src="../../images/cloud.svg"
-        alt="cloud-svg"
-      /> */}
+        src="../../images/cloud.png"
+        alt="구름"
+      />
       <StaticImage
         className="white-cloud"
         src="../../images/white-cloud.png"
-        alt="cloud-svg"
+        alt="하얀구름"
       />
-      {/* <StaticImage
+      <StaticImage
         className="white-cloud-2"
-        src="../../images/white-cloud.svg"
-        alt="cloud-svg"
-      /> */}
+        src="../../images/white-cloud.png"
+        alt="하얀구름"
+      />
     </div>
   )
 }
