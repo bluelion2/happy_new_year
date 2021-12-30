b6cb92544a83bead385ec43eefb2b15981839e77
ksh <iltk2000@naver.com>
2021-12-30 12:40:43 +0900

    [test] test png

--- src/components/wind/index.js
+++ src/components/wind/index.js
@@ -5,17 +5,7 @@ import "./index.css"
 const Wind = () => {
   return (
     <div className="wind">
-      <StaticImage
-        className="wind-1"
-        src="../../images/wind.svg"
-        alt="wind-svg"
-      />
-      <StaticImage
-        className="wind-2"
-        src="../../images/wind.svg"
-        alt="wind-svg"
-      />
-      <StaticImage
+      {/* <StaticImage
         className="wind-3"
         src="../../images/wind.svg"
         alt="wind-svg"
@@ -39,17 +29,17 @@ const Wind = () => {
         className="cloud-3"
         src="../../images/cloud.svg"
         alt="cloud-svg"
-      />
+      /> */}
       <StaticImage
         className="white-cloud"
-        src="../../images/white-cloud.svg"
+        src="../../images/white-cloud.png"
         alt="cloud-svg"
       />
-      <StaticImage
+      {/* <StaticImage
         className="white-cloud-2"
         src="../../images/white-cloud.svg"
         alt="cloud-svg"
-      />
+      /> */}
     </div>
   )
 }
