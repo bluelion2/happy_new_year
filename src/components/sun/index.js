b17e3598e51e8a888d387c2fdb23223f85e359af
ksh <iltk2000@naver.com>
2021-12-30 14:16:53 +0900

    [fix] cloud animation 추가

--- src/components/sun/index.js
+++ src/components/sun/index.js
@@ -1,8 +1,19 @@
 import React from "react"
+import { StaticImage } from "gatsby-plugin-image"
 import "./index.css"
 
 const Sun = () => {
-  return <div className="sun" />
+  return (
+    <div className="sun-wrap">
+      <div className="sun" />
+      <StaticImage className="cloud" src="../../images/cloud.png" alt="구름" />
+      <StaticImage
+        className="white-cloud"
+        src="../../images/white-cloud.png"
+        alt="구름"
+      />
+    </div>
+  )
 }
 
 export default React.memo(Sun)
